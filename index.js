2f121c721bd8ce659d0dda54ef9370536f77c5b8
Jobayer Rahman <jobayerrahman.diu@gmail.com>
2026-08-17 20:17:07 +0600

    counter js and type js

---
+++ index.js
@@ -0,0 +1,20 @@
+// Type JS
+
+let type = document.querySelector(".type")
+let typeContent = type.innerHTML
+type.innerHTML = ""
+
+let count = -1
+function typejs(){
+    count ++
+    type.innerHTML += typeContent.charAt(count)
+    if(count == typeContent.length){
+        type.innerHTML = ""
+        count = -1
+        // clearInterval(stoped)
+    }
+};
+
+let stoped =  setInterval(() => {
+    typejs()
+}, 500);
---
+++ script.js
@@ -0,0 +1,22 @@
+// Counter JS
+
+let counter = document.querySelectorAll(".counter")
+let arr = Array.from(counter)
+
+arr.map((item)=>{   
+    let userNumber = item.innerHTML
+    item.innerHTML = ""
+
+    let count = 0
+    function counterjs() {
+        count++
+        item.innerHTML = count
+        if(userNumber == count){
+            clearInterval(stopInterval)
+        }
+    };
+
+    let stopInterval = setInterval(() => {
+        counterjs()
+    }, 1000);
+});
